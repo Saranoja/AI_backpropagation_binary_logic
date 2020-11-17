cb07393a654043bcdc05aada89aefeb3b1fb23fb
Saranoja <Saranoja@users.noreply.github.com>
2020-11-17 13:08:51 +0200

    feat: added network skeleton

---
+++ NeuralNetwork.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class NeuralNetwork:
+    # TODO: init the layers and weights
+    def __init__(self, learning_rate=0.2):
+        pass
+
+    def activate(self):
+        pass
+
+    def activate_derivative(self):
+        pass
+
+    def get_sigmoid(self):
+        pass
+
+    def get_sigmoid_derivative(self):
+        pass
+
+    def forward_propagation(self):
+        pass
+
+    def backward_propagation(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+from NeuralNetwork import *
+
+# TODO: define X, y
+# TODO: train the net for a number of iterations
+# TODO: after training, predict the values for X
+if __name__ == '__main__':
+    pass
